a2e2aa813c171d34da21520a046dee085066ddd5
Mert Kureş <90699562+MertKures@users.noreply.github.com>
2023-01-20 23:18:29 +0300

    every search happens on background now. Added new feature "search on popup open"

--- src/background/background.js
+++ src/background/background.js
@@ -1,16 +1,15 @@
-//const utils = require('../../modules/utils.js');
+import { getDefaultSettings, initializeSettings } from 'modules/utils.js';
+let settings = getDefaultSettings();
 
-const defaultSettings = { modifiers: ["none"], default: "tdk", searchSelectedTextOnPopupShow: false };
-
-let settings = defaultSettings;
-
-async function loadSettings() {
-    settings = await browser.storage.local.get();
-
-    checkSettings();
-}
-
-loadSettings();
+//#region TEST
+initializeSettings()
+    .then(_settings => settings = _settings)
+    // TEST: [Search after popup open]: Select appropriate settigns to true.
+    .then(async (obj) => {
+        await browser.storage.local.set({ "searchSelectedTextOnPopupShow": true });
+        settings.searchSelectedTextOnPopupShow = true;
+    });
+//#endregion
 
 let waitingBrowserActionToSendAWord = false;
 let objectToSendToBrowserAction = null;
@@ -19,37 +18,20 @@ let objectToSendToBrowserAction = null;
 let portbA = null;
 
 //Sends meaning of the word that came from context menu.
-async function sendWordToTab(info, tab) {
-    let searchFrom = settings["default"];
-
-    if (searchFrom === "tdk") {
-        SearchFromTDK({ message: info.selectionText, isContextMenu: true }, { tab: tab });
-    } else if (searchFrom === "google") {
-        let text = await SearchFromGoogle(info.selectionText);
-
-        browser.tabs.sendMessage(tab.id, { type: "response_from_google", doc: text, isContextMenu: true });
-
-        text = null;
-    }
-
-    searchFrom = null;
+function sendContextMenuSelectedTextToTab(info, tab) {
+    browser.tabs.sendMessage(tab.id, { type: "SearchFromContextMenu", word: info.selectionText.trim().toLocaleLowerCase(), isContextMenu: true });
 }
 
 //async olursa browserAction açılmıyor.
 function ContextMenuClicked(info, tab) {
-    //Content script'e kutucuğu kapatmasını söylüyor.
-    browser.tabs.sendMessage(tab.id, { type: "SearchFromContextMenu" });
-
     if (info.menuItemId == "cm_onpopup") {
         waitingBrowserActionToSendAWord = true;
 
-        objectToSendToBrowserAction = { message: { modifiers: info.modifiers, word: info.selectionText.trim() }, type: "SearchFromContextMenu", searchEngine: settings["default"] };
+        objectToSendToBrowserAction = { word: info.selectionText.trim(), type: "SearchFromContextMenu", searchEngine: settings["default"] };
 
         browser.browserAction.openPopup();
-
-    } else if (info.menuItemId == "cm_onpage") {
-        sendWordToTab(info, tab);
-    }
+    } else if (info.menuItemId == "cm_onpage")
+        sendContextMenuSelectedTextToTab(info, tab);
 }
 
 let parentContextMenuOptions = {
@@ -60,16 +42,19 @@ let parentContextMenuOptions = {
     icons: { "32": browser.runtime.getURL("icons/32/icon.png") }
 };
 
-//Object.getPrototypeOf(error).name === "TypeError" && error.toString().includes("Unexpected property: 'icons'")
-
 try {
     browser.contextMenus.create(parentContextMenuOptions);
 } catch (hata) {
     delete parentContextMenuOptions.icons;
 
-    browser.contextMenus.create(parentContextMenuOptions);
+    try {
+        browser.contextMenus.create(parentContextMenuOptions);
 
-    console.log("Successfully created context menu !");
+        console.debug("Successfully created context menu !");
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
 browser.contextMenus.create({
@@ -86,188 +71,114 @@ browser.contextMenus.create({
     contexts: ["selection"]
 });
 
-browser.contextMenus.onClicked.addListener(ContextMenuClicked);
-
-function sendMessageToTab(message, sender) {
-    browser.tabs.sendMessage(sender.tab.id, message);
-}
-
-// function makeSettingsDefault() {
-//     browser.storage.local.set(defaultSettings);
-//     settings = defaultSettings;
-//     return defaultSettings;
-// }
-
-function makeSettingsDefault(...properties) {
-    if (properties.length === 0) {
-        settings = defaultSettings;
-        browser.storage.local.set(defaultSettings);
-        return defaultSettings;
-    }
-
-    for (property of properties) {
-        if (defaultSettings[property] == null)
-            continue;
-
-        settings[property] = defaultSettings[property];
-
-        let propertyObj = {};
-        propertyObj[property] = defaultSettings[property];
-
-        browser.storage.local.set(propertyObj);
-    }
-
-    return settings;
-}
-
 function updateSettings(e) {
-    let _settings = e;
-
-    //browser.storage.local.onChanged sadece değişen key ve value'yu verdiğinden varsayılan üstüne yazıyoruz.
-
-    for (key in _settings) {
-        settings[key] = _settings[key].newValue;
-    }
-
-    _settings = null;
+    for (const key in e)
+        settings[key] = e[key].newValue;
 }
 
-function checkSettings() {
-    if (!settings) {
-        settings = makeSettingsDefault();
-        console.debug("Settings, nesne boş olduğundan varsayılana ayarlandı.");
+async function SearchFromTDK(message, sender) {
+    let word = message.word ?? message;
+
+    let response = await fetch("https://sozluk.gov.tr/gts?ara=" + word, {
+        method: 'GET',
+        credentials: 'omit'
+    }).catch(error => console.error(error));
+
+    if (!response)
+        return;
+    else if (!response.ok) {
+        console.error(response.status, response.statusText);
         return;
     }
 
-    if (!(settings.default === "tdk" || settings.default === "google")) {
-        settings = makeSettingsDefault("default");
-        console.debug("'default' değeri boş olduğundan varsayılana ayarlandı.");
-    }
-
-    if ((Array.isArray(settings.modifiers)) ? settings.modifiers.length === 0 : true) {
-        settings = makeSettingsDefault("modifiers");
-        console.debug("'modifiers' dizisi boş olduğundan varsayılana ayarlandı.");
-    }
-
-    if (typeof settings.searchSelectedTextOnPopupShow !== "boolean") {
-        settings = makeSettingsDefault("searchSelectedTextOnPopupShow");
-        console.debug("'searchSelectedTextOnPopupShow' değeri boş olduğundan varsayılana ayarlandı.");
-    }
-}
-
-function SearchFromTDK(message, sender) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState != XMLHttpRequest.DONE || !(xhr.status == 0 || (xhr.status >= 200 && xhr.status < 400)))
-            return;
-
-        var list = JSON.parse(xhr.responseText);
-
-        //Hata oluştuysa bitir.
-        if (!list) {
-            console.error("list -> undefined");
-            return;
-        }
-        else if (list.error) {
-            console.error("Sonuç bulunamadı");
-            return;
-        }
-
-        list = list[0];
-        //Anlam var ise devam et.
-        if (parseInt(list.anlam_say) < 1) {
-            return;
-        }
-
-        var anlamlarListe = [];
-        var orneklerListe = [];
-
-        list.anlamlarListe.forEach(a => {
-            anlamlarListe.push({
-                anlam: (a.anlam) ? a.anlam : "",
-                fiil: (a.fiil) ? a.fiil : "",
-                isim: (a.ozelliklerListe) ? (a.ozelliklerListe[0]) ? (a.ozelliklerListe[0].tam_adi) ? a.ozelliklerListe[0].tam_adi : "" : "" : ""
-            });
-            if (a.orneklerListe) {
-                a.orneklerListe.forEach(b => {
-                    orneklerListe.push({
-                        ornek: (b.ornek) ? b.ornek : "",
-                        yazar: (b.yazar) ? (b.yazar[0]) ? (b.yazar[0].tam_adi) ? b.yazar[0].tam_adi : "" : "" : ""
-                    });
-                });
-            }
-        });
-
-        list = null;
-
-        sendMessageToTab({
-            type: "response_from_tdk",
-            anlamlarListe: anlamlarListe,
-            orneklerListe: orneklerListe,
-            x: message.x,
-            y: message.y,
-            word: message.message,
-            isContextMenu: message.isContextMenu
-        }, sender);
-
-        //xhr null yapınca null diye hata veriyor.
-        //xhr = null;
-    }
-    xhr.open("GET", "https://sozluk.gov.tr/gts?ara=" + message.message);
-    xhr.send();
-}
-
-//Sadece 2 cümleye ulaşabiliyoruz.
-function SearchFromGoogle(word) {
-    //"https://www.google.com/search?q=" + word + "+ne+demek"
+    let list;
 
     try {
-        return fetch("https://www.google.com/search?q=" + word + "+ne+demek", {
-            method: 'GET',
-            credentials: 'omit'
-        })
-            .then((response) => response.text())
-            .then((text) => {
-                //Klonlanamaz hatası verdiğinden kapattım
-                //const _document = new DOMParser().parseFromString(text, 'text/html');
-                //return _document;
-
-                return text;
-            }).catch(hata => {
-                console.error(hata);
-            });
+        list = await response.json();
     } catch (error) {
         console.error(error);
     }
+
+    //Hata oluştuysa bitir.
+    if (!list) {
+        console.error("list -> undefined");
+        return;
+    }
+    else if (list.error) {
+        console.error(list.error);
+        return;
+    }
+
+    list = list[0];
+
+    //Anlam var ise devam et.
+    if (parseInt(list.anlam_say) < 1) {
+        console.error("Anlam listesi boştu.");
+        return;
+    }
+
+    let anlamlarListe = [];
+    let orneklerListe = [];
+
+    list.anlamlarListe.forEach(a => {
+        anlamlarListe.push({
+            anlam: (a.anlam) ? a.anlam : "",
+            fiil: (a.fiil) ? a.fiil : "",
+            isim: (a.ozelliklerListe) ? (a.ozelliklerListe[0]) ? (a.ozelliklerListe[0].tam_adi) ? a.ozelliklerListe[0].tam_adi : "" : "" : ""
+        });
+        if (a.orneklerListe) {
+            a.orneklerListe.forEach(b => {
+                orneklerListe.push({
+                    ornek: (b.ornek) ? b.ornek : "",
+                    yazar: (b.yazar) ? (b.yazar[0]) ? (b.yazar[0].tam_adi) ? b.yazar[0].tam_adi : "" : "" : ""
+                });
+            });
+        }
+    });
+
+    list = null;
+
+    return {
+        type: "response_from_tdk",
+        anlamlarListe: anlamlarListe,
+        orneklerListe: orneklerListe,
+        word: word,
+    };
+}
+
+//Sadece 2 cümleye ulasabiliyoruz.
+async function SearchFromGoogle(word) {
+    //"https://www.google.com/search?q=" + word + "+ne+demek"
+
+    let response = await fetch("https://www.google.com/search?q=" + word + "+ne+demek", {
+        method: 'GET',
+        credentials: 'omit'
+    }).catch(err => { console.error(err) });
+
+    if (!response)
+        return;
+    else if (!response.ok) {
+        console.error("Word: " + word, "Result: " + response.status + " " + response.statusText);
+        return;
+    }
+
+    return { type: "response_from_google", doc: await response.text() };
+}
+
+async function Search(message, sender) {
+    if (message == null)
+        return;
+
+    let _searchEngine = message.searchEngine ?? settings["default"];
+
+    if (_searchEngine === "tdk") return await SearchFromTDK(message, sender);
+    else if (_searchEngine === "google") return await SearchFromGoogle(message.word);
+    else return;
 }
 
 async function onMessage(message, sender) {
-    /*
-    tdk -> message -> {
-        message: "kazı",
-        x: 15,
-        y: 50,
-        type: "search_from_tdk"
-    }
-
-    google -> message -> {
-        message: "kazı",
-        x: 15,
-        y: 50,
-        type: "response_from_google",
-        doc: string HTMLDocument
-    }
-    */
-
-    if (message.type === "search_from_tdk") {
-        SearchFromTDK(message, sender);
-    } else if (message.type === "search_from_google") {
-        let text = await SearchFromGoogle(message.message);
-
-        sendMessageToTab({ type: "response_from_google", doc: text, x: message.x, y: message.y }, sender);
-    }
-
-    return Promise.resolve();
+    if (message.type === "Search")
+        return Promise.resolve(Search(message, sender));
 }
 
 function onConnect(port) {
@@ -278,8 +189,6 @@ function onConnect(port) {
             portbA = null;
         }
 
-        //console.debug(port, "browserAction portu bağlandı.");
-
         portbA = port;
 
         portbA.name = "bA";
@@ -287,10 +196,13 @@ function onConnect(port) {
         if (waitingBrowserActionToSendAWord && objectToSendToBrowserAction) {
             portbA.postMessage(objectToSendToBrowserAction);
 
-            //console.debug(objectToSendToBrowserAction, "mesajı browserAction'a gönderildi.");
-
             waitingBrowserActionToSendAWord = false;
             objectToSendToBrowserAction = null;
+        }
+        else {
+            portbA.postMessage({
+                type: "PopupConnectedToBackground"
+            });
         }
 
         portbA.onDisconnect.addListener(onDisconnect);
@@ -300,14 +212,10 @@ function onConnect(port) {
 }
 
 function onDisconnect(port) {
-    //console.log(port, "browserAction portunda bağlantı koptu.", "Hata : " + ((port.error) ? port.error : "Yok"));
+    console.debug(port, "'" + port.name + "' portuyla bağlantı koptu.", (port.error) ? "Error: " + port.error : "");
 
-    console.log(port, "'" + port.name + "' portuyla bağlantı koptu.", (port.error) ? "Error: " + port.error : "");
-
-    if (port.name === "bA") {
+    if (port.name === "bA")
         portbA = null;
-        return;
-    }
 }
 
 browser.runtime.onConnect.addListener(onConnect);
@@ -315,3 +223,5 @@ browser.runtime.onConnect.addListener(onConnect);
 browser.runtime.onMessage.addListener(onMessage);
 
 browser.storage.onChanged.addListener(updateSettings);
+
+browser.contextMenus.onClicked.addListener(ContextMenuClicked);
